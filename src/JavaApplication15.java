f461f8698e779e2c12545979dd757610a9384cd4
PaveL <BPaveL07@yandex.ru>
2021-09-06 21:51:13 +0300

    1111

--- src/JavaApplication15.java
+++ src/JavaApplication15.java
@@ -5,7 +5,36 @@ public class JavaApplication15 {
 
     public static void main(String[] args) {
 
-        ArrayList 
+        ArrayList<Number> list = new ArrayList<>();
+
+        list.add(2);
+        list.add(3.6);
+
+        for (Number number: list){
+            System.out.println(number);
+        }
+
+        ArrayList<ArrayList<Number>> bigList = new ArrayList<>();
+        bigList.add(list);
+        bigList.get(0).add(10);
+        bigList.add(new ArrayList<>());
+        bigList.get(1).add(11);
+
+
+        for (ArrayList<Number> arrayList : bigList){
+            for(Number number: arrayList){
+                System.out.println(number + " ");
+            }
+
+            System.out.println();
+        }
+
+        ArrayList<Comparable<Double>> cList = new ArrayList<>();
+
+        cList.add(10.0);
+        cList.add(2.5);
+
+        if(cList.get(0).compareTo((Double) cList.get(1))>0) System.out.println("Ok");
 
     }
 
